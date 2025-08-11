dd2c7bbe40598b7af688eea5282aa190caf03b24
Victor Pyatakov <viktor.pyatakov@tiyn.io>
2025-08-11 10:09:10 +0300

    fixes

--- gateways/mobile/src/api/identity_srv/v1/auth/auth.module.ts
+++ gateways/mobile/src/api/identity_srv/v1/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { JwtService, JwtModule } from '@nestjs/jwt';
 
 import { AccessTokenStrategy } from './strategies/access-token.strategy';
