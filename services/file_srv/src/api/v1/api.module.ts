a953f6745af5a5bc99f9e9ceedff0f60dbd700d7
Victor Pyatakov <viktor.pyatakov@tiyn.io>
2026-06-27 13:13:21 +0300

    extract file service

--- services/file_srv/src/api/v1/api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { FolderModule } from './folder/folder.module';
-import { FileModule } from './file/file.module';
-
-@Module({
-  imports: [FolderModule, FileModule],
-})
-export class ApiV1Module {}
